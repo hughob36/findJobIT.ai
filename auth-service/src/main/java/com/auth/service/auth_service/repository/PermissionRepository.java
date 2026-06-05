00bdb40810a5e5ff54c0d950851e8a384f942a98
Hugo Benitez <hugo.boca16@gmail.com>
2026-06-05 18:15:55 -0300

    repository

---
+++ auth-service/src/main/java/com/auth/service/auth_service/repository/PermissionRepository.java
@@ -0,0 +1,9 @@
+package com.auth.service.auth_service.repository;
+
+import com.auth.service.auth_service.model.Permission;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PermissionRepository extends JpaRepository<Permission, Long> {
+}
---
+++ auth-service/src/main/java/com/auth/service/auth_service/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.auth.service.auth_service.repository;
+
+import com.auth.service.auth_service.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
